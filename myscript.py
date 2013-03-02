d825ad8f618e175094e1a5404c564098aa0fd295
chiyx <chiyxme@outlook.com>
2013-03-02 13:51:28 +0800

    char4 script

--- myscript.py
+++ myscript.py
@@ -20,6 +20,13 @@ def fib2(n): #return Fibonacci series up to n
         a, b = b, a + b
     return result
 
-#call the function fib2
-f100 = fib2(1000)
-print f100
+# keyword argument function
+def parrot(voltage, state = 'a stiff', action = 'voom', type = 'Norwegian Blue'):
+    print "-- This parrot would't", action
+    print "if you put ", voltage, " volts through it."
+    print "-- Lovely plumag, the ", type
+    print "-- It's ", state, "!"
+
+parrot(1000)
+parrot(voltage = 1000)
+parrot(voltage = 100000, action = 'VOOOOM')
