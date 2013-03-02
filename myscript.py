dd8da9843869942ab09e846ee099cedb1552d086
chiyx <chiyxme@outlook.com>
2013-03-02 13:31:56 +0800

    char4 script

--- myscript.py
+++ myscript.py
@@ -3,3 +3,23 @@
 
 currency=u"гд"
 print ord(currency)
+
+def fib(n): #write Fibonacci series up to n
+    """Print a Fibonacci series up to n """
+    a, b = 0, 1
+    while a  < n:
+        print a,
+        a, b = b, a + b
+
+def fib2(n): #return Fibonacci series up to n
+    """Return a list containing the Fibonacci series up to n."""
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a + b
+    return result
+
+#call the function fib2
+f100 = fib2(1000)
+print f100
