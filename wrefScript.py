474c86b26740e7ee1645abeb7afb1c1e95da2734
chiyx <chiyxme@outlook.com>
2013-03-06 00:17:35 +0800

    language 3

---
+++ wrefScript.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+import weakref, gc
+class A:
+    def __init__(self, value):
+        self.value = value
+    def __repr__(self):
+        return str(self.value)
+
+a = A(10)
+d = weakref.WeakValueDictionary()
+d['primary'] = a
+print d['primary']
+del a
+print gc.collect()
+
+print d['primary']
+
+
