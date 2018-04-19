2ef3e3940ce027770a4e2a572dfd61034e0fb8c8
chiyx <chiyxme@outlook.com>
2018-04-19 23:41:08 +0800

    fluent_python - ch06

---
+++ src/block_chain/blockchain.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+
+
+class Blockchain:
+
+    def funcname(self, parameter_list):
+        pass
---
+++ src/fluent_python/ch06/strategy_simple.py
@@ -0,0 +1,50 @@
+# -*- coding: UTF-8 -*-
+
+from abc import ABC, abstractmethod
+from collections import namedtuple
+
+Customer = namedtuple('Customer', 'name fidelity')
+
+
+class LineItem:
+
+    def __init__(self, product, quantity, price):
+        self.product = product
+        self.quantity = quantity
+        self.price = price
+
+    def total(self):
+        return self.price * self.quantity
+
+
+class Order:
+
+    def __init__(self, customer, cart, promotion=None):
+        self.customer = customer
+        self.cart = list(cart)
+        self.promotion = promotion
+
+    def total(self):
+        if not hasattr(self, '__total'):
+            self.__total = sum(item.total() for item in self.cart)
+        return self.__total
+
+    def dup(self):
+        if self.promotion is None:
+            discount = 0
+        else:
+            discount = self.promotion.discount(self)
+        return self.total() - discount
+
+    def __repr__(self):
+        fmt = '<Order total: {:.2f} due: {:.2f}>'
+        return fmt.format(self.total(), self.dup())
+
+
+class Promotion(ABC):
+
+    @abstractmethod
+    def discount(self, order):
+        """
+        返回折扣金额(正值)
+        """
