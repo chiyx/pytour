7696baf4310f5a82fe3cd8834e95fd083262a263
chiyx <chiyxme@outlook.com>
2018-05-10 00:33:18 +0800

    fluentPython - ch07 deco

---
+++ src/fluent_python/ch07/clock_deco.py
@@ -0,0 +1,38 @@
+# -*- coding: UTF-8 -*-
+
+import time
+import functools
+
+
+def clock(func):
+    @functools.wraps(func)
+    def clocked(*args, **kwargs):
+        t0 = time.time()
+        result = func(*args, **kwargs)
+        elapsed = time.time() - t0
+        name = func.__name__
+        arg_lst = []
+        if args:
+            arg_lst.append(', '.join(repr(arg) for arg in args))
+        if kwargs:
+            pairs = ['%s=%r' % (k, w) for k, w in sorted(kwargs.items())]
+            arg_lst.append(', '.join(pairs))
+        arg_str = ', '.join(arg_lst)
+        print('[%0.8fs] %s(%s) -> %r ' % (elapsed, name, arg_str, result))
+        return result
+
+    return clocked
+
+
+@functools.lru_cache()
+@clock
+def factorial(n):
+    return 1 if n < 2 else n * factorial(n - 1)
+
+
+def main():
+    factorial(7)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/fluent_python/ch07/generic.py
@@ -0,0 +1,30 @@
+# -*- coding: UTF-8 -*-
+
+from functools import singledispatch
+from collections import abc
+import numbers
+import html
+
+
+@singledispatch
+def htmlize(obj):
+    content = html.escape(repr(obj))
+    return '<pre>{}</pre>'.format(content)
+
+
+@htmlize.register(str)
+def _(text):
+    content = html.escape(text).replace('\n', '<br>\n')
+    return '<p>{}</p>'.format(content)
+
+
+@htmlize.register(numbers.Integral)
+def _(n):
+    return '<pre>{0} (0x{0:x})</pre>'.format(n)
+
+
+@htmlize.register(tuple)  # <5>
+@htmlize.register(abc.MutableSequence)
+def _(seq):
+    inner = '</li>\n<li>'.join(htmlize(item) for item in seq)
+    return '<ul>\n<li>' + inner + '</li>\n</ul>'
